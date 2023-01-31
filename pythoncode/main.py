c2fe8f88c5f46a7f02cae526e65449cc828e1a47
milomolly <91715261+milomolly@users.noreply.github.com>
2023-01-31 22:22:38 +0700

    Add files via upload

---
+++ pythoncode/main.py
@@ -0,0 +1,22 @@
+import json
+
+f = open('py_challange_input.json')
+
+data = json.load(f)
+outcomelist = []
+iMatches = 0
+for i in data['Cricket']:
+    for j in i['formats']:
+        for k in j['regions']:
+            for l in k['matches']:
+                outcomeData = {
+                    "match_id" : l['id'],
+                    "team1" : l['team1'],
+                    "team2" : l['team2'],
+                    "httpLink" : l['httpLink']
+                }
+                outcomeData['livestream_provider'] = l['streaming']['liveStream']['provider']
+                outcomeData['winner'] = l['results']['winner']
+            outcomeData["regionName"] = k['name']
+            print(outcomeData);
+f.close()
